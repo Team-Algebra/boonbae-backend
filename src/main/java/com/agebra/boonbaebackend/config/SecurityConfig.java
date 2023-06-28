6554d8ad19bee871be69276447fedd25e4eda3f4
Juhyorim <joohr1234@gmail.com>
2023-06-28 23:09:07 +0900

    fest: 회원 엔티티 추가, jwt추가

---
+++ src/main/java/com/agebra/boonbaebackend/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.agebra.boonbaebackend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private String[] whiteList = {"/api/v1/register", "/api/v1/authenticate", "/swagger-ui/index.html"};
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers(whiteList)
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+            .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
