0fcefeaee23d6141f024fe67afba66eff8d5f52c
Seol_JY <wlsdud5654@gmail.com>
2023-06-28 17:12:21 +0900

    feat: 데이터베이스 연동

---
+++ src/main/java/com/agebra/boonbaebackend/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.agebra.boonbaebackend.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+
+    public String first() {
+        return "index";
+    }
+
+}
