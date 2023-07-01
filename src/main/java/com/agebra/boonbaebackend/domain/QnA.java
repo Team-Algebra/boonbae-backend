24ba7506e5ccf4f5370c2faa6182a8f86a76dc56
lhetl <93638355+lhetl@users.noreply.github.com>
2023-07-01 15:19:14 +0900

    feat: QnA 수정, 답변 수정 추가 fix: pk notnull 수정, localdatetime docs: 필드 이름 수정
    domain

--- src/main/java/com/agebra/boonbaebackend/domain/QnA.java
+++ src/main/java/com/agebra/boonbaebackend/domain/QnA.java
@@ -1,12 +1,14 @@
 package com.agebra.boonbaebackend.domain;
 
+import com.agebra.boonbaebackend.exception.ForbiddenException;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
 @Builder
 @Data
@@ -16,7 +18,6 @@ import java.time.LocalDate;
 @Table(name="qna")
 public class QnA {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NotNull
     @Column(name="pk")  //기본키
     private Long pk;
 
@@ -40,17 +41,43 @@ public class QnA {
     @NotNull
     @CreationTimestamp
     @Column(name="create_data")
-    private LocalDate createDate=LocalDate.now();
+    private LocalDateTime createDate=LocalDateTime.now();
 
     @Column(name="reply_text", length=999999999) //답변이 달리면 Null이 아님 -> 답변완료
-    private String replyText;
+    private String replyText=null;
 
-    public static QnA makeQnA(Users user, QnAType qnAType, String title, String description) {
+    public static QnA makeQnA(Users user, QnAType qnaType, String title, String description) {
         QnA qna= new QnA();
         qna.user = user;
-        qna.qnaType = qnAType;
+        qna.qnaType = qnaType;
         qna.title = title;
         qna.descriptions = description;
         return qna;
     }
+    public void editQnA(Users user, QnAType qnaType, String title, String description){
+        if(this.user.getPk()==user.getPk()){
+            this.qnaType=qnaType;
+            this.title=title;
+            this.descriptions=description;
+        }else{
+            throw new ForbiddenException("QnA를 작성한 유저가 아닙니다.");
+        }
+    }
+    public void makeReply(String replyText){
+        this.replyText=replyText;
+    }
+
+    public void editReply(String replyText) {
+        if (this.replyText == null) {
+            throw new ForbiddenException("답변을 먼저 작성해주세요");
+
+        } else {
+            this.replyText = replyText;
+        }
+    }
+    public String getUserName(){
+        return this.user.getUsername();
+    }
+
+
 }
