e90a55c3d91a2619d51c51e3d820f457b46a6fad
lhetl <93638355+lhetl@users.noreply.github.com>
2023-07-01 15:19:36 +0900

    feat: JpaRepository 추가
    Repository

---
+++ src/main/java/com/agebra/boonbaebackend/repository/QnARepository.java
@@ -0,0 +1,7 @@
+package com.agebra.boonbaebackend.repository;
+
+import com.agebra.boonbaebackend.domain.QnA;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QnARepository extends JpaRepository<QnA,Long> {
+}
