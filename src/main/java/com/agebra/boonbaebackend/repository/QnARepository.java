a146247bc1f34e0c7a89e0e7601c250dd7c4c2e5
lhetl <hyun91000@gmail.com>
2023-07-01 22:07:12 +0900

    feat: QnAType(category) 검색 추가

--- src/main/java/com/agebra/boonbaebackend/repository/QnARepository.java
+++ src/main/java/com/agebra/boonbaebackend/repository/QnARepository.java
@@ -1,7 +1,12 @@
 package com.agebra.boonbaebackend.repository;
 
 import com.agebra.boonbaebackend.domain.QnA;
+import com.agebra.boonbaebackend.domain.QnAType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QnARepository extends JpaRepository<QnA,Long> {
+//    @Query("SELECT q FROM QnA q order by q.qnaType")
+    List<QnA> findByQnaType(QnAType qnaType);
 }
