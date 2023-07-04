3c4edf3988ded5bce7500d79aedee16f520e4894
lhetl <hyun91000@gmail.com>
2023-07-04 16:07:46 +0900

    feat: 첫번째 카테고리로 두번째 카테고리 찾기 기능 추가

--- src/main/java/com/agebra/boonbaebackend/repository/SecondCategoryRepository.java
+++ src/main/java/com/agebra/boonbaebackend/repository/SecondCategoryRepository.java
@@ -4,6 +4,7 @@ import com.agebra.boonbaebackend.domain.funding.FirstCategory;
 import com.agebra.boonbaebackend.domain.funding.SecondCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SecondCategoryRepository extends JpaRepository<SecondCategory, Long> {
@@ -11,4 +12,5 @@ public interface SecondCategoryRepository extends JpaRepository<SecondCategory, 
   Optional<SecondCategory> findByName(String name);
 
   Optional<SecondCategory> findByFirstCategoryAndName(FirstCategory firstCategory, String name);
+  List<SecondCategory> findAllByFirstCategory(FirstCategory firstCategory);
 }
