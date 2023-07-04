b1e7960e0c01458e5e2b1252d8766441c0fbca9c
Seol_JY <wlsdud5654@gmail.com>
2023-07-04 20:31:09 +0900

    feat: vaildate 구현 완료

--- src/main/java/com/agebra/boonbaebackend/config/SecurityConfig.java
+++ src/main/java/com/agebra/boonbaebackend/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
       "/api/v1/search/ranking",
       "/api/v1/recycling/*", "/api/v1/tip/",
       "/api/v1/comments/*/reports",
-      "/api/v1/recycling/*"
+      "/api/v1/recycling/*", "/api/v1/token/validate"
     };
 
     private String[] adminList = {
--- src/main/java/com/agebra/boonbaebackend/controller/TokenController.java
+++ src/main/java/com/agebra/boonbaebackend/controller/TokenController.java
@@ -1,9 +1,13 @@
 package com.agebra.boonbaebackend.controller;
 
+import com.agebra.boonbaebackend.domain.Users;
 import com.agebra.boonbaebackend.dto.UserDto;
+import com.agebra.boonbaebackend.service.JwtService;
 import com.agebra.boonbaebackend.service.UserService;
+import io.jsonwebtoken.Jwt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -16,11 +20,16 @@ import java.util.Map;
 public class TokenController {
 
   private final UserService userService;
+  private final JwtService jwtService;
 
   @GetMapping("/token/validate")
-  public ResponseEntity<Map> validateJwtToken(@RequestBody String request) {
+  public ResponseEntity<Map<String, Object>> validateJwtToken(@AuthenticationPrincipal Users user) {
+    Map<String, Object> response = new HashMap<>();
 
-    return ResponseEntity.ok().build();
+    boolean isValid = jwtService.isTokenValidByUsers(user);
+
+    response.put("isValid", isValid);
+    return ResponseEntity.ok(response);
   }
 
 }
