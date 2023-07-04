931f6400256b3150c7272d300a2cd5dfd2f77700
lhetl <93638355+lhetl@users.noreply.github.com>
2023-07-04 22:05:53 +0900

    feat: 유저로 펀딩 찾기 기능 추가

--- src/main/java/com/agebra/boonbaebackend/repository/FundingRepository.java
+++ src/main/java/com/agebra/boonbaebackend/repository/FundingRepository.java
@@ -1,7 +1,12 @@
 package com.agebra.boonbaebackend.repository;
 
 import com.agebra.boonbaebackend.domain.Funding;
+import com.agebra.boonbaebackend.domain.FundingLike;
+import com.agebra.boonbaebackend.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FundingRepository extends JpaRepository<Funding, Long> {
+    List<Funding> findByUser(Users user);
 }
