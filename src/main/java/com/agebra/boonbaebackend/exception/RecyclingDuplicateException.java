53a0b621a3e4d02e3dd434e2af1887410a7a0244
eunslog <yeseo235@gmail.com>
2023-07-07 22:11:13 +0900

    fix/분리배출 정보 등록 - 중복 여부 확인 추가 test/RecyclingService

---
+++ src/main/java/com/agebra/boonbaebackend/exception/RecyclingDuplicateException.java
@@ -0,0 +1,11 @@
+package com.agebra.boonbaebackend.exception;
+
+public class RecyclingDuplicateException extends RuntimeException{
+    public RecyclingDuplicateException() {
+        super();
+    }
+    public RecyclingDuplicateException(String message) {
+        super(message);
+    }
+
+}
