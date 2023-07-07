e6c6c598875bc59b1908489a33ef591a4aaf6990
Juhyorim <joohr1234@gmail.com>
2023-07-08 02:17:46 +0900

    fix: 분리배출 등록 2 result 에러 처리

--- src/main/java/com/agebra/boonbaebackend/repository/RecyclingTypeRepository.java
+++ src/main/java/com/agebra/boonbaebackend/repository/RecyclingTypeRepository.java
@@ -3,11 +3,12 @@ package com.agebra.boonbaebackend.repository;
 import com.agebra.boonbaebackend.domain.RecyclingType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface RecyclingTypeRepository extends JpaRepository<RecyclingType, Long> {
 
-    Optional<RecyclingType> findByName(String type);
+    Optional<List<RecyclingType>> findByName(String type);
 
 }
--- src/main/java/com/agebra/boonbaebackend/repository/TagRepository.java
+++ src/main/java/com/agebra/boonbaebackend/repository/TagRepository.java
@@ -3,9 +3,11 @@ package com.agebra.boonbaebackend.repository;
 import com.agebra.boonbaebackend.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
-    Tag findByName(String tag);
+    List<Tag> findByName(String tag);
 
 }
